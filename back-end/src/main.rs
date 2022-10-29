7098f48a875d9fb9fa508a83875cb8e74a721bc1
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-10-29 12:49:27 +0900

    ✨ 雑に db に接続できるように

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,4 +1,11 @@
+use std::env;
+
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use once_cell::sync::OnceCell;
+use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
+
+pub static POOL: OnceCell<MySqlPool> = OnceCell::new();
 
 #[get("/")]
 async fn hello_world() -> impl Responder {
@@ -7,8 +14,31 @@ async fn hello_world() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let hostname = env::var("MARIADB_HOSTNAME").unwrap();
+    let database = env::var("MARIADB_DATABASE").unwrap();
+    let username = env::var("MARIADB_USERNAME").unwrap();
+    let password = env::var("MARIADB_PASSWORD").unwrap();
+
+    let pool = MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&format!(
+            "mysql://{}:{}@{}/{}",
+            username, password, hostname, database
+        ))
+        .await
+        .unwrap();
+    POOL.set(pool).unwrap();
+
+    let query = "SHOW TABLES;";
+    let tables = sqlx::query(query)
+        .fetch_all(&*POOL.get().unwrap())
+        .await
+        .unwrap();
+    println!("Tables: {:?}", tables);
+
     HttpServer::new(|| App::new().service(hello_world))
-        .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
