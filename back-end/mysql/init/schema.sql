2d0934d98c2adb1e843d22f062d3247f1c696512
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-11-04 12:01:16 +0900

    🚧 ♻️ DB 定義で enum 使うのやめた
    ref: https://shiro-secret-base.com/?p=928

--- back-end/mysql/init/schema.sql
+++ back-end/mysql/init/schema.sql
@@ -21,6 +21,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS `state_mapping` (
+  `state_id` TINYINT NOT NULL,
+  `state_name` varchar(255) NOT NULL UNIQUE,
+  PRIMARY KEY (`state_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `state_mapping` (`state_id`, `state_name`) VALUES
+  (0, 'icebox'),
+  (1, 'todo'),
+  (2, 'in-progress'),
+  (3, 'done')
+ON DUPLICATE KEY UPDATE state_id=state_id;
+
+CREATE TABLE IF NOT EXISTS `priority_mapping` (
+  `priority_id` TINYINT NOT NULL,
+  `priority_name` varchar(255) NOT NULL UNIQUE,
+  PRIMARY KEY (`priority_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `priority_mapping` (`priority_id`, `priority_name`) VALUES
+  (0, 'low'),
+  (1, 'medium'),
+  (2, 'high')
+ON DUPLICATE KEY UPDATE priority_id=priority_id;
+
 CREATE TABLE IF NOT EXISTS `todos` (
   `id` VARBINARY(16) NOT NULL,
   `author_id` VARBINARY(16),
@@ -29,12 +52,15 @@ CREATE TABLE IF NOT EXISTS `todos` (
   `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-  `state` ENUM('icebox', 'todo', 'in-progress', 'done') NOT NULL DEFAULT 'todo',
-  `priority` ENUM('low', 'medium', 'high'),
+  `state` VARCHAR(255) NOT NULL DEFAULT 'todo',
+  `priority` VARCHAR(255),
   `due_date` DATETIME,
 
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE SET NULL
+  FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE SET NULL,
+
+  FOREIGN KEY (`state`) REFERENCES `state_mapping` (`state_name`) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (`priority`) REFERENCES `priority_mapping` (`priority_name`) ON UPDATE CASCADE ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `todo_taggings` (
